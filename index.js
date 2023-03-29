ee19c51993053c72fa75b448e95a37437c094060
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-03-29 13:32:24 -0400

    exercise 3.9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { response, request } = require("express");
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token("bodyContent", (req, res) => JSON.stringify(req.body));
 
