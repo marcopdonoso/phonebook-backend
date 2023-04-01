f6d6b0466115aeda5de0cfe282a518f3f9f9f7c6
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-04-01 00:15:31 -0400

    exercise 3.15 - 3.18

--- index.js
+++ index.js
@@ -19,10 +19,6 @@ app.use(
 	)
 );
 
-app.get("/", (request, response) => {
-	response.send("<h1>Hello World!</h1>");
-});
-
 app.get("/api/persons", (request, response) => {
 	Person.find({}).then((persons) => {
 		response.json(persons);
@@ -43,10 +39,14 @@ app.get("/api/persons/:id", (request, response) => {
 	Person.findById(request.params.id).then((person) => response.json(person));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-	const id = Number(request.params.id);
-	persons = persons.filter((p) => p.id !== id);
-	response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+	Person.findByIdAndRemove(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => {
+			next(error);
+		});
 });
 
 app.post("/api/persons", (request, response) => {
@@ -67,6 +67,36 @@ app.post("/api/persons", (request, response) => {
 		response.json(personSaved);
 	});
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+	const body = request.body;
+	const person = {
+		name: body.name,
+		number: body.number,
+	};
+
+	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+		.then((personUpdated) => response.json(personUpdated))
+		.catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error);
+
+	if (error.name === "CastError") {
+		response.status(400).send({ error: "malformatted Id" });
+	}
+
+	next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
